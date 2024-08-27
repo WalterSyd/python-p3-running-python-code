370785e0f24c7e5b7f48428df2c2d4b186a2df21
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-08-27 11:55:24 +0300

    Tests passed

---
+++ lib/app.py
@@ -0,0 +1,17 @@
+# name = input("What is your name? ")
+# print(name)
+
+# price_1 = 3
+# price_2 = 2.5
+
+# print(f"item1 costs ${price_1}")
+# print(f"item2 costs ${price_2}")
+# print(f"total costs ${price_1 + price_2}")
+
+# print(len([1,3,400,7]))
+# print(sorted([5,100,234,7,2]))
+# list_123 = [1, 2, 3]
+# print(list_123.pop())
+# print (list_123)
+
+print("Hello World! Pass this test, please.")
